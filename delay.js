4bb3ed65b1e99052bbd8ed37cb432b214384c8b9
Matt Deeds <mdeeds@gmail.com>
2024-02-02 10:40:53 -0800

    Tidy up audio and latencies

--- delay.js
+++ delay.js
@@ -1,7 +1,17 @@
 async function getAudioSourceNode() {
     const audioCtx = new AudioContext();
     // Request access to the default audio input and create the source node
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const stream = await navigator.mediaDevices.getUserMedia({
+  audio: {
+    // Other desired audio constraints
+    mandatory: {
+      // Disable specific processing features
+      echoCancellation: false,
+      noiseSuppression: false,
+      autoGainControl: false, // Not guaranteed to work in all browsers
+    },
+  },
+});
     const source = audioCtx.createMediaStreamSource(stream);
     return source;
 }
@@ -44,21 +54,46 @@ async function createAnalyzerAndVisualizer(sourceNode) {
   return canvas; // Return the canvas element for display
 }
 
+function getLatencyFromURL() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const latency = urlParams.get('latency');
+
+  if (latency) {
+	return Number(latency);
+  } else {
+    return 0.0;
+  }
+}
 
 class DelayTrack {
     constructor(source) {
-        // Create the DelayNode with an 8-second delay and 100% feedback
+		console.log(`Estimated output latency: ${source.context.outputLatency}`);
+		
+		let latency = (2.0 * source.context.outputLatency) + getLatencyFromURL();
+		
+		let latencyInSamples = source.context.sampleRate * latency;
+		console.log(`Number of samples: ${latencyInSamples}`);
+		
+		latencyInSamples = Math.round(latencyInSamples);
+		latency = latencyInSamples / source.context.sampleRate;
+		
         const delayNode = new DelayNode(source.context, {
-            delayTime: 8,
+            delayTime: 8 - latency,
             maxDelayTime: 32,
         });
 
-        source.connect(delayNode);
+		
+	 const internalDelay = new DelayNode(source.context, {
+		 delayTime: latency,
+	 maxDelayTime: latency});
+
+        source.connect(delayNode);	
 
         createAnalyzerAndVisualizer(source)
             .then((node) => { document.body.appendChild(node); });
         
-        delayNode.connect(delayNode);
+        delayNode.connect(internalDelay);
+		internalDelay.connect(delayNode);
 
         delayNode.connect(source.context.destination);
 
