80043e692f260a6e84ac90c9ced5491a81394b5e
mdeeds <mdeeds@penguin>
2024-02-17 10:22:49 -0800

    Factor out util code

--- fragment-shader.glsl
+++ fragment-shader.glsl
@@ -5,36 +5,24 @@ varying vec2 v_position;
 uniform float u_canvasWidth;
 uniform float u_canvasHeight;
 
-vec3 sphere(in vec2 look, in vec2 pos, in float radius) {
-  vec2 delta = look - pos;
-  if (length(delta) < radius) {
-    vec3 surface = vec3(delta.xy, sqrt(abs(radius * radius - delta.x * delta.x - delta.y * delta.y)));
-    surface = normalize(surface);
-    return vec3(0.05  / (surface.z + 0.001));
-  } else {
-    return vec3(0.0, 0.0, 0.0);
-  }
-}
-
-
-vec3 hsv2rgb(vec3 c)
-{
-    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-}
-
 vec4 bg(in vec2 canvasPos) {
-  vec2 origin = vec2(0.5, -0.5);
-  float len = length(canvasPos - origin);
+  vec2 origin = vec2(0.5, -1.0);
+  vec2 relPos = (canvasPos - origin);
+  if (abs(relPos.y / relPos.x) < 4.0) {
+    // return vec4(noise1(vec3(canvasPos, 0.0)), 1.0);
+    return vec4(0.0, 0.0, 0.0, 1.0);
+    // return vec4(hsv2rgb(vec3(length(noise2(vec3(canvasPos.xy, 0.0))), 0.05, 1.0)), 1.0);
+  }
+  
+  float len = length(relPos);
 
-  float bottom = 1.0;
-  float top = 1.3;
+  float bottom = 1.5;
+  float top = 1.8;
   float height = top - bottom;
   
   if (len < bottom || len > top) return vec4(1.0, 1.0, 1.0, 1.0);
   
-  return vec4(hsv2rgb(vec3(-2.0 * (len - bottom) / height, 0.5, 1.0)), 1.0);
+  return vec4(hsv2rgb(vec3(-2.0 * (len - bottom) / height, 0.1, 1.0)), 1.0);
 }
 
 void main() {
---
+++ util.glsl
@@ -0,0 +1,38 @@
+precision mediump float; // You can choose between lowp, mediump, or highp
+
+vec3 sphere(in vec2 look, in vec2 pos, in float radius) {
+  vec2 delta = look - pos;
+  if (length(delta) < radius) {
+    vec3 surface = vec3(delta.xy, sqrt(abs(radius * radius - delta.x * delta.x - delta.y * delta.y)));
+    surface = normalize(surface);
+    return vec3(0.05  / (surface.z + 0.001));
+  } else {
+    return vec3(0.0, 0.0, 0.0);
+  }
+}
+
+
+vec3 noiseG(in vec3 p) {
+  return vec3(0.123 * sin(p.y + 193.2) + 0.312 * sin(p.z * 2.0 + 482.1),
+              0.234 * sin(p.z + 412.3) + 0.731 * sin(p.x * 2.0 + 846.1),
+              0.521 * sin(p.x + 438.1) + 0.132 * sin(p.y * 2.0 + 174.1));
+}
+
+vec3 noise1(in vec3 p) {
+  p = p * 3.1415;
+  return vec3(sin(2.0 * p.y + 312.3) + sin(3.0 * p.z + 842.1),
+              sin(2.0 * p.z + 432.1) + sin(3.0 * p.x + 492.2),
+              sin(2.0 * p.x + 531.7) + sin(3.0 * p.y + 573.3));
+}
+
+vec3 noise2(in vec3 p) {
+  return noise1(noise1(noise1(p)));
+}
+
+
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
