91b2a3b469e73da98e68a5041e9e95312a6879c1
mdeeds <mdeeds@penguin>
2024-02-17 09:08:34 -0800

    Add screen coordinates

--- fragment-shader.glsl
+++ fragment-shader.glsl
@@ -2,7 +2,8 @@ precision mediump float; // You can choose between lowp, mediump, or highp
 
 varying vec2 v_position;
 
-
+uniform float u_canvasWidth;
+uniform float u_canvasHeight;
 
 vec3 sphere(in vec2 look, in vec2 pos, in float radius) {
   vec2 delta = look - pos;
@@ -13,16 +14,18 @@ vec3 sphere(in vec2 look, in vec2 pos, in float radius) {
   } else {
     return vec3(0.0, 0.0, 0.0);
   }
-  
 }
 
+vec4 bg(in vec2 canvasPos) {
+  return vec4(canvasPos.xy, 1.0, 1.0);
+}
 
 void main() {
-  // gl_FragColor = vec4(0.0, 1.0, 0.2, 1.0);
-  // vec4 background = vec4(v_position.x + 1.0, v_position.y + 1.0, 1.0, 1.0);
-  vec4 background = vec4(1.0, 1.0, 1.0, 1.0);
-
-  vec3 foreground = sphere(v_position, vec2(0, 0), 0.5);
+  vec2 canvasPos = gl_FragCoord.xy / vec2(u_canvasWidth, u_canvasHeight);
+  
+  vec4 background = bg(canvasPos); // vec4(1.0, 1.0, 1.0, 1.0);
+  
+  vec3 foreground = sphere(gl_FragCoord.xy, vec2(300.0, 300.0), 50.0);
 
   gl_FragColor = background - vec4(foreground, 0.0);
 }
--- vertex-shader.glsl
+++ vertex-shader.glsl
@@ -1,8 +1,12 @@
 // precision mediump float; // You can choose between lowp, mediump, or highp
 attribute vec4 a_position;
+attribute vec4 a_screen;
+
 varying vec2 v_position;
+varying vec2 v_screen;
 
 void main() {
   gl_Position = a_position;
   v_position = a_position.xy;
+  v_screen = a_position.xy;
 }
