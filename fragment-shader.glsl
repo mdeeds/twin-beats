6930abe675a3b4fa306d4594b08bbe78670dda79
mdeeds <mdeeds@penguin>
2024-02-24 17:50:08 -0800

    A little bit of junk code to convert from XY to Note and pan.

--- fragment-shader.glsl
+++ fragment-shader.glsl
@@ -6,31 +6,41 @@ uniform float u_canvasWidth;
 uniform float u_canvasHeight;
 uniform vec4 u_bubbleLocations[64];
 
-  
-vec4 bg(in vec2 canvasPos) {
-  vec2 origin = vec2(0.5, -1.0);
-  vec2 relPos = (canvasPos - origin);
-  if (abs(relPos.y / relPos.x) < 4.0) {
-    // return vec4(noise1(vec3(canvasPos, 0.0)), 1.0);
-    return vec4(0.0, 0.0, 0.0, 1.0);
-    // return vec4(hsv2rgb(vec3(length(noise2(vec3(canvasPos.xy, 0.0))), 0.05, 1.0)), 1.0);
-  }
-  
-  float len = length(relPos);
+// TODO: We either need to cap this at ~900 or send this as a texture.
+// uniform float u_spectrograms[16 * 1024];
+uniform float u_panning[16];
 
-  float bottom = 1.5;
-  float top = 1.8;
-  float height = top - bottom;
-  
-  if (len < bottom || len > top) return vec4(1.0, 1.0, 1.0, 1.0);
-  
-  return vec4(hsv2rgb(vec3(-2.0 * (len - bottom) / height, 0.1, 1.0)), 1.0);
+
+vec4 spectrogram(in float panning, in float spectrogram[1024]) {
+  return vec4(0.0);
 }
 
+
+vec4 bg(in vec2 posPanNote, in vec2 originPanNote) {
+  float c = 0.9 + 0.05 * sin(length(posPanNote - originPanNote) / 12.0 * 3.1415 * 2.0);
+  return vec4(c, c, c, 1.0);
+}
+
+#define BOTTOM_NOTE 21.0
+#define TOP_NOTE 129.0
+
 void main() {
-  vec2 canvasPos = gl_FragCoord.xy / vec2(u_canvasWidth, u_canvasHeight);
+  vec2 posXY = gl_FragCoord.xy;
+
+  vec2 bottomCenterPanNote = vec2(0.0, BOTTOM_NOTE);
+  vec2 topCenterPanNote = vec2(0.0, TOP_NOTE);
+  vec2 originPanNote = vec2(0.0, BOTTOM_NOTE - 0.5 * (TOP_NOTE - BOTTOM_NOTE));
   
-  vec4 background = bg(canvasPos); // vec4(1.0, 1.0, 1.0, 1.0);
+  vec2 bottomCenterXY = vec2(u_canvasWidth / 2.0, 0.0);
+  vec2 topCenterXY = vec2(u_canvasWidth / 2.0, u_canvasHeight);
+  vec2 originXY = vec2(u_canvasWidth / 2.0, -u_canvasHeight / 2.0);
+
+  vec2 relXY = posXY - originXY;
+  vec2 posPanNote = relXY / length(topCenterXY - bottomCenterXY) * length(topCenterPanNote - bottomCenterPanNote);
+  
+  
+  
+  vec4 background = bg(posPanNote, originPanNote);
   
   vec3 foreground = vec3(0.0);
 
