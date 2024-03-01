fd1aa5369d04f4ca6d17f2c3a74b200cd312ec48
mdeeds <mdeeds@penguin>
2024-03-01 13:24:54 -0800

    Use linear interpolation to smooth out spectrogram

--- webgl.js
+++ webgl.js
@@ -78,8 +78,8 @@ setUpTexture = async function(gl, program) {
     // Create a texture for storing data
     const texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     const textureWidth = 1024; // Size of the FFT frequency data
